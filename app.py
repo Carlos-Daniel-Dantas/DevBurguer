27feca0493fc9e0641f1bec70f588f9a43ed6467
Carlos Daniel <carlos.d.dantas6@aluno.senai.br>
2026-04-02 17:00:54 -0300

    ADD: Usando extends para passar layouts, criando web servicee banco de dados com informações das compras

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_principal():
+
+    return render_template("index.html")
+@app.route("/pagina2")
+def pagina_pagina2():
+
+    return render_template("pagina2.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ migrate/estrutura_banco.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS dev_burguer;
+use dev_burguer;
+
+CREATE TABLE hamburguer (
+	codigo INT AUTO_INCREMENT PRIMARY KEY,
+    produto VARCHAR(200),
+    descricao VARCHAR(200),
+    precor FLOAT,
+    destaque VARCHAR(20),
+    foto VARCHAR(255),
+    disponibilidade VARCHAR(20)
+    );
