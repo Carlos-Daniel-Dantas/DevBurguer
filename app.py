5977cfac07f9f140f92fc4cbc0d5dcf51ffd234c
Carlos Daniel <carlos.d.dantas6@aluno.senai.br>
2026-04-09 17:20:52 -0300

    ADD: Adicionando jinja para pegar os elementos do banco de dados e ajustanto alguns erros

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template, request, redirect
-import mysql.connector
+from flask import Flask, render_template
+from model.produto import capturando_produtos
 
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_principal():
+    produtos = capturando_produtos()
+    return render_template("index.html", produtos = produtos)
 
-    return render_template("index.html")
+# @app.route("/layout")
+# def layout():
+#     return render_template("layout.html")
+
 @app.route("/pagina2")
 def pagina_pagina2():
-
     return render_template("pagina2.html")
 
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
-
