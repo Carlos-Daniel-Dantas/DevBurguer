248a5a85fc3725d172aa7cef53084ca71a16d41b
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-04-10 11:26:20 -0300

    Criando funções de recuperar produto para pagina 2 e função de select para fazer o item aparecer em destaques

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template
-from model.produto import capturando_produtos
+from model.produto import capturando_produtos, capturando_destaques, capturando_produto
 
 app = Flask(__name__)
 
 @app.route("/")
 def pagina_principal():
+
     produtos = capturando_produtos()
-    return render_template("index.html", produtos = produtos)
+    destaques = capturando_destaques()
 
-# @app.route("/layout")
-# def layout():
-#     return render_template("layout.html")
+    return render_template("index.html", produtos = produtos, destaques = destaques)
 
-@app.route("/pagina2")
-def pagina_pagina2():
-    return render_template("pagina2.html")
 
+
+
+@app.route("/pagina2/<codigo>")
+def pagina_pagina2(codigo):
+
+    recuperar_produto = capturando_produto(codigo)
+
+    return render_template("pagina2.html", recuperar_produtoproduto = recuperar_produto)
 
 
 if __name__=="__main__":
--- model/produto.py
+++ model/produto.py
@@ -11,3 +11,42 @@ def capturando_produtos():
     conexao.close()
 
     return produto
+
+def capturando_destaques():
+
+    conexao, cursor = conectar()
+
+    cursor.execute("SELECT codigo, produto, foto FROM produtos WHERE destaque = 1;")
+
+    resultado = cursor.fetchall()
+
+    conexao.close()
+
+    return resultado
+
+def capturando_produto(codigo: int):
+
+    conexao, cursor = conectar()
+
+    cursor.execute("""
+                   SELECT " \
+                    "codigo, " \
+                    "produto, " \
+                    "descricao, " \
+                    "preco, " \
+                    "destaque, " \
+                    "foto, " \
+                    "disponibilidade " \
+                    "FROM produtos " \
+                    "WHERE codigo = %s;
+                   """,
+                   
+                   [codigo])
+
+
+
+    produto = cursor.fetchone()
+
+    conexao.close()
+
+    return produto
