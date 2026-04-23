f1389080a90edb36d45c08c164bbf860212b45cf
Carlos Dantas <carlos.d.dantas6@aluno.senai,br>
2026-04-23 17:27:48 -0300

    Add: Função cadastrar e login de úsuario feita, também carrinho de itens inserido usando js.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
-from model.produto import capturando_produtos, capturando_destaques, capturando_produto
+from flask import Flask, render_template, request, redirect, session
+from model.produto import capturando_produtos, capturando_destaques, capturando_produto 
+from model.usuarios import Usuarios
 
 app = Flask(__name__)
+
+app.secret_key = "banana_loca"
 
 @app.route("/")
 def pagina_principal():
@@ -11,15 +14,47 @@ def pagina_principal():
 
     return render_template("index.html", produtos = produtos, destaques = destaques)
 
-
-
-
 @app.route("/pagina2/<codigo>")
 def pagina_pagina2(codigo):
 
     rcp = capturando_produto(codigo)
 
     return render_template("pagina2.html", rcp = rcp)
+
+@app.route("/cadastrar", methods=["GET"])
+def cadastrar():
+    
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+    nome = request.form.get ("nome")
+
+    novo_usuario = Usuarios(usuario, senha, nome)
+    novo_usuario.cadastrar()
+
+    return render_template("/login.html")
+
+@app.route("/login/usuario", methods=["POST"])
+def pagina_login():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+
+    resultado = usuario.logar(usuario, senha)
+    return render_template("login.html")
+
+    if not resultado:
+        session["usuario_logado"] = resultado
+        return redirect()
+    
+@app.route('/carrinho')
+def ver_carrinho():
+    # Pega a lista de produtos da sessão, ou uma lista vazia se não houver nada
+    itens = session.get('carrinho', [])
+    
+    # Calcula o total (convertendo para float se necessário)
+    total = sum(float(str(item['preco']).replace(',', '.')) for item in itens)
+    
+    return render_template('carrinho.html', carrinho=itens, total=f"{total:.2f}".replace('.', ','))
+
 
 
 if __name__=="__main__":
--- migrate/estrutura_banco.sql
+++ migrate/estrutura_banco.sql
@@ -5,7 +5,7 @@ CREATE TABLE produtos (
 	codigo INT AUTO_INCREMENT PRIMARY KEY,
     produto VARCHAR(200),
     descricao VARCHAR(200),
-    precor FLOAT,
+    preco FLOAT,
     destaque VARCHAR(20),
     foto VARCHAR(255),
     disponibilidade VARCHAR(20)
@@ -13,3 +13,10 @@ CREATE TABLE produtos (
 
     insert into produtos(produto, descricao, preco, destaque, foto, disponibilidade)
     values("Godo Burguer", "Um lanche especial espera por voce com uma carne divina", 24.00, 1, "https://assets.grok.com/users/6c51eb9c-5b1a-4715-b68c-9f325c88541d/generated/82bc4c98-4a2d-4e4f-b243-0fcce9c83917/image.jpg", 1);
+
+    CREATE TABLE IF NOT EXISTS usuarios (
+    nome VARCHAR(100) DEFAULT "ANONIMO",
+    usuario VARCHAR(100) NOT NULL PRIMARY KEY,
+    senha VARCHAR(255) NOT NULL
+);
+
