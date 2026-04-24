77c3cfa0b508b13384838e665d587cd15ff78566
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-04-24 11:27:10 -0300

    ADD: Inner join feito entre as tabelas

--- model/carrinho.py
+++ model/carrinho.py
@@ -5,7 +5,17 @@ def recuperar_carrinho( usuario:str )-> list:
     conexao, cursor = conectar()
 
     cursor.execute("""
-                    select * from itens_carrinho;
+                    select carrinhos.cod_carrinho,
+                        usuarios.usuario,
+                        carrinhos.data,
+                        carrinhos.finalizado,
+                        produtos.produto,
+                        itens_carrinho.quantidade,
+                        produtos.preco,
+                        produtos.foto
+                    From carrinhos
+                    inner join itens_carrinho on carrinhos.cod_carrinho = itens_carrinho.cod_carrinho
+                    inner join  produtos on produto.codigo = itens_carrinho.cod_produto
                    """)
 
     produto = cursor.fetchall()
